4ca4a2457d6cfa3785379010f770c549de2851b7
Christian Lachmann <christian-lachmann@arcor.de>
2021-11-05 17:19:18 +0100

    created device repository without functionality

---
+++ SampleTaskWeb.DAL/DeviceRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleTaskWeb.Server.Repositories;
+using SampleTaskWeb.Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleTaskWeb.DAL
+{
+  /// <summary>
+  /// Repository for managing devices
+  /// </summary>
+  public class DeviceRepository : IGenericRepository<Device>
+  {
+    private readonly DbContext _dbContext;
+
+    public DeviceRepository(DbContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+    /// <inheritDoc/>
+    public Task<Device> AddAsync(Device entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <inheritDoc/>
+    public Task Delete(Device entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <inheritDoc/>
+    public Task<IEnumerable<Device>> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Shared/Device.cs
@@ -0,0 +1,53 @@
+﻿namespace SampleTaskWeb.Shared
+{
+  /// <summary>
+  /// Represents a device
+  /// </summary>
+  public class Device
+  {
+    /// <summary>
+    /// Name of the device
+    /// </summary>
+    public string DeviceName { get; set; }
+
+    /// <summary>
+    /// Indicates whether the device is failsafe
+    /// </summary>
+    public bool FailSafe { get; set; }
+
+    /// <summary>
+    /// The device's type id
+    /// </summary>
+    public string DeviceTypeId { get; set; }
+
+    /// <summary>
+    /// Minimal operating temperature
+    /// </summary>
+    public int TempMin { get; set; }
+
+    /// <summary>
+    /// Maximal operating temperature
+    /// </summary>
+    public int TempMax { get; set; }
+
+    /// <summary>
+    /// Indicates the installation position of the device (horizontal or vertical)
+    /// </summary>
+    public string InstallationPosition { get; set; }
+
+    /// <summary>
+    /// Indicates if the device fits in a 19 inch cabinet
+    /// </summary>
+    public bool InsertInto19InchCabinet { get; set; }
+
+    /// <summary>
+    /// Indicates if the device is a terminal element
+    /// </summary>
+    public bool? TerminalElement { get; set; }
+
+    /// <summary>
+    /// Indicates if the device can work in advanced environmental conditions
+    /// </summary>
+    public bool? AdvancedEnvironmentConditions { get; set; }
+  }
+}
